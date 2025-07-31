2f3301717125c354aa437cbc3a47ea060c7d9763
Juka-coder <pilaojxjd@gmail.com>
2025-07-30 21:38:05 -0300

    Service layer, component registration

--- src/main/java/com/github/juka_coder/crud_spring/repositories/UserRepository.java
+++ src/main/java/com/github/juka_coder/crud_spring/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package com.github.juka_coder.crud_spring.repositories;
 import com.github.juka_coder.crud_spring.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<User, Long>{
 
 }

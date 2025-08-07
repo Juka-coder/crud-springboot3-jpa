0edcc6017190b93e81e501fb1e283e43141bf453
Juka-coder <pilaojxjd@gmail.com>
2025-08-06 22:09:07 -0300

    Payment, one-to-one association

--- src/main/java/com/github/juka_coder/crud_spring/entities/Order.java
+++ src/main/java/com/github/juka_coder/crud_spring/entities/Order.java
@@ -31,6 +31,8 @@ public class Order implements Serializable {
     @JoinColumn(name = "client_id")
     private User client;
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
 
     public Order() {
     }
@@ -40,6 +42,14 @@ public class Order implements Serializable {
         this.moment = moment;
         this.client = client;
         setOrderStatus(orderStatus);
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     public Long getId() {
